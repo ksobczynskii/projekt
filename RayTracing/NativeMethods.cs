8f639e1ed60d62b3b19cdcda04f5491860627201
Konstanty Sobczynski <01199334@pw.edu.pl>
2026-02-09 12:17:21 +0100

    initial commit

---
+++ RayTracing/NativeMethods.cs
@@ -0,0 +1,9 @@
+using System.Runtime.InteropServices;
+using Microsoft.Win32.SafeHandles;
+
+namespace RayTracing;
+
+internal static partial class NativeMethods
+{
+    private const string LibName = "rt";
+}
---
+++ RayTracing/native/export.cpp
@@ -0,0 +1,13 @@
+#include "rtweekend.h"
+#include "hittable_list.h"
+#include "material.h"
+#include "sphere.h"
+#include "color.h"
+#include "camera.h"
+#include <vector>
+#include <cstring>
+#include <memory>
+#include <cstdint>
+
+#define STB_IMAGE_WRITE_IMPLEMENTATION
+#include "external/stb_image_write.h"
